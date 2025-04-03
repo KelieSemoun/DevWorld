3d41a8451135ccbd0bc21d19278674fe7179949f
Kevin <kevinelie1998@gmail.com>
2025-04-03 11:23:59 +0200

    Unsubscribe to topics implemented

--- back/src/main/java/com/openclassrooms/mddapi/controllers/UserController.java
+++ back/src/main/java/com/openclassrooms/mddapi/controllers/UserController.java
@@ -1,12 +1,16 @@
 package com.openclassrooms.mddapi.controllers;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.openclassrooms.mddapi.dto.TopicDTO;
 import com.openclassrooms.mddapi.services.UserService;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -19,16 +23,27 @@ public class UserController {
 	public UserController(UserService userService) {
 		this.userService = userService;
 	}
+	
+	@GetMapping("/{userId}/topics")
+	public ResponseEntity<List<TopicDTO>> getSubscribedTopics(@PathVariable Integer userId) {
+		return ResponseEntity.ok(userService.getSubscribedTopics(userId));
+	}
 
-	@PostMapping("{id}/subscribe/{topicId}")
-	public ResponseEntity<?> subscribeToTopic(@PathVariable("id") String id, @PathVariable("topicId") String topicId){
+	@PostMapping("{userId}/subscribe/{topicId}")
+	public ResponseEntity<?> subscribeToTopic(@PathVariable Integer userId, @PathVariable Integer topicId){
 		try {
-			this.userService.subscribeToTopic(Integer.parseInt(id), Integer.parseInt(topicId));
+			this.userService.subscribeToTopic(userId, topicId);
 			
 			return ResponseEntity.ok().build();
 		} catch (NumberFormatException e) {
 			return ResponseEntity.badRequest().build();
 		}
 		
+	}
+	
+	@PostMapping("/{userId}/unsubscribe/{topicId}")
+	public ResponseEntity<Void> unsubscribe(@PathVariable Integer userId, @PathVariable Integer topicId) {
+	    userService.unsubscribe(userId, topicId);
+	    return ResponseEntity.ok().build();
 	}
 }
--- back/src/main/java/com/openclassrooms/mddapi/services/UserService.java
+++ back/src/main/java/com/openclassrooms/mddapi/services/UserService.java
@@ -1,10 +1,14 @@
 package com.openclassrooms.mddapi.services;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
+import com.openclassrooms.mddapi.dto.TopicDTO;
 import com.openclassrooms.mddapi.exception.NotFoundException;
+import com.openclassrooms.mddapi.mappers.TopicMapper;
 import com.openclassrooms.mddapi.models.Topic;
 import com.openclassrooms.mddapi.models.User;
 import com.openclassrooms.mddapi.repository.TopicRepository;
@@ -17,9 +21,31 @@ public class UserService {
 
 	private final TopicRepository topicRepository;
 	
-	public UserService(UserRepository userRepository, TopicRepository topicRepository) {
+	private final TopicMapper topicMapper;
+	
+	public UserService(UserRepository userRepository, TopicRepository topicRepository, TopicMapper topicMapper) {
 		this.userRepository = userRepository;
 		this.topicRepository = topicRepository;
+		this.topicMapper = topicMapper;
+	}
+	
+	public List<TopicDTO> getSubscribedTopics(Integer userId) {
+	    User user = userRepository.findById(userId)
+	        .orElseThrow(() -> new NotFoundException());
+	    
+	    return user.getTopics().stream()
+	        .map(topicMapper::toDto)
+	        .collect(Collectors.toList());
+	}
+
+	public void unsubscribe(Integer userId, Integer topicId) {
+	    User user = userRepository.findById(userId)
+	        .orElseThrow(() -> new NotFoundException());
+	    Topic topic = topicRepository.findById(topicId)
+	        .orElseThrow(() -> new NotFoundException());
+
+	    user.getTopics().remove(topic);
+	    userRepository.save(user);
 	}
 
 	public void subscribeToTopic(int id, int topicId) {
